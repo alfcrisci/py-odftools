7a8f31ee3c2b073b3149899a4c3b39593dbe11aa
eric.talevich <eric.talevich@4df8ec11-d81d-0410-9be8-316d5d3e3d15>
2006-10-10 07:11:06 +0000

    odf & document: Document object will use minidom for now; clarified read/write interface somewhat; a hint of a test script in odf.py.

--- odftools/document.py
+++ odftools/document.py
@@ -2,31 +2,55 @@
 
 """ document
 
-The document object, to be used as the intermediate step for all operations
+The document object, for containing the document in memory and to be used as 
+the intermediate step for all operations.
+
+This implementation relies on minidom for handling the object. It may later
+become prudent to use the bulkier xml.dom or pyxml, but for now, this
+should do.
 """
 
-from xml import sax, dom
+from xml.dom.minidom import *
 
 class Document:
-	""" The ODF document object.
-
-	Three potential approaches:
-	1. sax -- store the attributes as strings, parse as needed.
-	2. dom -- parse into trees on initialization
-	3. pyxml -- outside library, not sure what they have going on
-	"""
+	""" The ODF document object. """
 
 	def __init__(self, 
-				 content='', 
-				 styles='', 
-				 meta='', 
-				 settings='', 
-				 mimetype=''):
-		self.content = content
-		self.styles = styles
-		self.meta = meta
-		self.settings = settings
+				 mimetype='',	# Mimetype string
+				 manifest=""	# Lists the contents of the ODF file
+				 content='',	# Content data (the text)
+				 styles='',		# Formatting data 
+				 settings='',	# Use is specific to the application 
+				 meta='',		# Metadata
+				 # Consider **kwords for META-INF
+				 ): 
 		self.mimetype = mimetype
+		self.manifest = xml.dom.minidom.parseString(manifest)
+		self.content = xml.dom.minidom.parseString(content)
+		self.styles = xml.dom.minidom.parseString(styles)
+		self.settings = xml.dom.minidom.parseString(settings)
+		self.meta = xml.dom.minidom.parseString(meta)
+		# --- alternate route --- 
+		# self.Components = {}
 
+	def __del__(self):
+		# unlink each DOM component
+		# for attr in []:
+		#	self.__attr__(attr).unlink()
+		self.manifest.unlink()
+		self.content.unlink()
+		self.styles.unlink()
+		self.settings.unlink()
+		self.meta.unlink()
+		
+
+	def getText(self, nodelist):
+		rc = []
+		for node in nodelist:
+			if node.nodeType == node.TEXT_NODE:
+				rc.append(node.data + '\n')
+		return "".join(rc)
+
+	
 
 #EOF
--- odftools/odf.py
+++ odftools/odf.py
@@ -2,74 +2,101 @@
 
 """ odf.py
 
+
+The full OASIS OpenDocument specification can be found here:
+http://www.oasis-open.org/specs/index.php#opendocumentv1.0
+
+Mad love to Bob Sutor (www.sutor.com). Bob's Dr. ODF project/blog series
+suggests the following features as examples:
+
+* Take an ODF document and extract any images/objects embedded in it.
+* Take an ODF document and throw away all formatting except the basic heading 
+	and paragraph structure, printing what’s left on the screen. (to text)
+* Iterate over a directory of ODF files and print how many of them were authored
+	by a given person. (list file ownership in a friendly format)
+* Take an ODF word processing document and convert it to HTML. (Don’t forget 
+	the images and tables!)
+* Extract all the elements of a given type (e.g. formulas, code) from an ODF
+	document.
+
+More ideas:
+* Clean up styles: If two or more styles have the same attributes, pick one 
+	name for all of them and delete the other styles.
+* Determine document type (in case of wrong filename extension)
+*
+
+
+
+
 """
 
 import zipfile
+from cStringIO import StringIO
 
-
-# Map the names of the Zipped files to attributes of the Document object.
-filenames = {"content.xml":content,
-			 "styles.xml":style,
-			 "meta.xml":meta,
-			 "settings.xml":settings } # mimetype?
 
 class WriteException: pass
 class ReadException: pass
 
 
-class odf:
-    """ Provides a Pickle-like interface for reading and writing. """
-    def __init__(self):
-		pass
+# Provides a Pickle-like interface for reading and writing. 
 
-	def dump(self, obj, dst, mode=None):
-        dst.write( OdfWriter.write(obj, mode) )
-        return
+def Load(src):
+	return _read(src)
 
-    def dumps(self, obj, mode=None):
-        dst = StringIO()
-        self.dump(obj, dst, mode)
-        return dst.getvalue()
+def Loads(str):
+	src = StringIO(str)
+	return _read(src)
 
-    def load(self, src):
-        return OdfReader.read(src)
+def Dump(obj, dst):
+	dst.write(_write(obj))
+	return
 
-    def loads(self, str):
-        src = StringIO(str)
-        return OdfReader.read(src)
+def Dumps(obj):
+	dst = StringIO()
+	dst.write(_write(obj))
+	return dst.getvalue()
 
 
-class OdfWriter:
-	""" Class for writing an ODF Document object to the output stream. """
+# The business end of this ---------------------------------------------------
 
-	def __init__(self):
-		pass
+# Map the names of the Zipped files to attributes of the Document object.
+g_Filenames = {"content.xml":content,
+			 "styles.xml":style,
+			 "meta.xml":meta,
+			 "settings.xml":settings } # mimetype?
 
-	def write(self, obj, dst)
-		""" Writes each of obj's attributes to a Zip file named dst. """
-		zf = ZipFile(dst, 'w')
-		for filename, attr in filenames:
-			f = open(filename, 'w')
-			f.write(obj.__getattr__(attr)) # Check syntax
-			zf.write(filename)
-			f.close()
-		zf.close()
+def _read(src):
+	""" Reads the contents of each of the files in archive src into obj. """
+	zf = ZipFile(src, 'r')
+	obj = None
+	for filename, component in g_Filenames:
+		obj.__setattr__(component, zf.read(filename))
+	zf.close()
+	return obj
+	
+
+def _write(obj, dst):
+	""" Writes each of obj's attributes to a Zip file named dst.
+
+	NB: Pardon the mess, this isn't going to be implemented till later.
+	"""
+	#out_stream = obj.toFile() # TODO: implement
+	zf = ZipFile(dst, 'w')
+	#zf.write(filename) # takes the name or an existing file
+	out_stream.close()
+	zf.close()
 
 
-class OdfReader:
-	""" Class for reading an ODF Document object from the input stream. """
+# --- Basic test script -------------------------------------------------------
 
-	def __init__(self):
-		pass
-
-	def read(self, src):
-		""" Read the contents of each of the files in archive src into obj. """
-		zf = ZipFile(src, 'r')
-		obj = None
-		for filename, attr in filenames:
-			obj.__setattr__(attr) = zf.namelist(filename).read() # Check syntax
-		zf.close()
-
+if __name__ == "__main__":
+	
+	import os
+	
+	for filename in os.listdir(os.getcwd()):
+		if filename.rsplit('.').pop() in ['odt']:
+			doc = Load(open(filename, 'r'))
+			print doc.getText()
 
 
 #EOF

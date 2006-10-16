093e94efb6e47c403895b4601067aceef2b7d9f1
eric.talevich <eric.talevich@4df8ec11-d81d-0410-9be8-316d5d3e3d15>
2006-10-16 05:14:40 +0000

    document: convert ODF to text and sort of to HTML. Also implemented writing the object to file (untested)

--- trunk/odftools/document.py
+++ trunk/odftools/document.py
@@ -6,51 +6,109 @@ The document object, for containing the document in memory and to be used as
 the intermediate step for all operations.
 
 This implementation relies on minidom for handling the object. It may later
-become prudent to use the bulkier xml.dom or pyxml, but for now, this
+make sense to use the bulkier xml.dom or pyxml, but for now, this
 should do.
 """
 
-from xml.dom.minidom import *
+import xml.dom.minidom as dom
 
 class Document:
 	""" The ODF document object. """
 
 	def __init__(self, 
 				 mimetype='',	# Mimetype string
-				 manifest=""	# Lists the contents of the ODF file
+				 manifest='',	# Lists the contents of the ODF file
 				 content='',	# Content data (the text)
 				 styles='',		# Formatting data 
 				 settings='',	# Use is specific to the application 
 				 meta='',		# Metadata
-				 # Consider **kwords for META-INF
+				 **kwords		# Other files in META-INF
 				 ): 
 		self.mimetype = mimetype
-		self.manifest = xml.dom.minidom.parseString(manifest)
-		self.content = xml.dom.minidom.parseString(content)
-		self.styles = xml.dom.minidom.parseString(styles)
-		self.settings = xml.dom.minidom.parseString(settings)
-		self.meta = xml.dom.minidom.parseString(meta)
+		self.manifest = dom.parseString(manifest)
+		self.content = dom.parseString(content)
+		self.styles = dom.parseString(styles)
+		self.settings = dom.parseString(settings)
+		self.meta = dom.parseString(meta)
+		#ENH: Handle **kwords
 		# --- alternate route --- 
 		# self.Components = {}
 
 	def __del__(self):
-		# unlink each DOM component
-		# for attr in []:
-		#	self.__attr__(attr).unlink()
+		""" Unlink each DOM component """
 		self.manifest.unlink()
 		self.content.unlink()
 		self.styles.unlink()
 		self.settings.unlink()
 		self.meta.unlink()
+
+
+	# Extract objects from the document --------------------------------------
+
+	def getComponentAsString(self, component_name):
+		nodelist = self.__getattr__(component_name)
+		return nodelist.toprettyxml()
+
+
+	def getEmbeddedObjects(self, filter=None):
+		""" Return a dictionary of the objects embedded in the document.
+
+		A more general form of getImages. By default, this should return
+		all embedded objects; the list/dictionary can also be filtered
+		for a certain type, e.g. image files.
+		"""
+		pass
 		
+	def getElementsByType(self, elementtype):
+		""" Extract all elements of a given type from the document.
 
-	def getText(self, nodelist):
-		rc = []
-		for node in nodelist:
+		For example, formulas or code.
+		"""
+		pass
+
+	# Convert the document to other formats ---------------------------------
+
+	def toText(self):
+		""" Returns the content of the document as a plain-text string. """
+		textlist = []
+		for node in self.content:
 			if node.nodeType == node.TEXT_NODE:
-				rc.append(node.data + '\n')
-		return "".join(rc)
+				textlist.append(node.data)
+		return "\n".join(textlist)
 
-	
+
+	def toHTML(self, title=""):
+		""" Returns an HTML representation of the document. 
+		
+		The current version of this function is essentially the same as toText.
+		
+		The next version should:
+		- Create a stylesheet in the header
+		- Apply corresponding style classes to each node
+		
+		After that, this function should also handle:
+		- hyperlinks
+		- images
+		- tables
+		"""
+		values = {}
+		values["Title"] = title		# Title for the page, if applicable
+		bodyList = []	# the lines to insert into the body of the document
+		# Not implemented yet:
+		metaList = [] 	# Meta tags for the header
+		styleList = []	# Stylesheet elements
+
+		# Extract the body of the HTML document
+		for node in self.content:
+			if node.nodeType == node.TEXT_NODE:
+				htmllist.append("<p>%s</p>" % node.data)
+		values["Body"] = "\n".join(htmllist)
+
+		# Apply values to the HTML template
+		f = open("template.html", 'r')
+		htmlTemplate = f.read()
+		f.close
+		return htmlTemplate % values
+
 
 #EOF
--- trunk/odftools/odf.py
+++ trunk/odftools/odf.py
@@ -6,30 +6,34 @@
 The full OASIS OpenDocument specification can be found here:
 http://www.oasis-open.org/specs/index.php#opendocumentv1.0
 
-Mad love to Bob Sutor (www.sutor.com). Bob's Dr. ODF project/blog series
+
+Bob Sutor's Dr. ODF project/blog series (http://www.sutor.com)
 suggests the following features as examples:
 
-* Take an ODF document and extract any images/objects embedded in it.
 * Take an ODF document and throw away all formatting except the basic heading 
-	and paragraph structure, printing what’s left on the screen. (to text)
+	and paragraph structure, printing what’s left on the screen. [toText]
+* Take an ODF word processing document and convert it to HTML. (Don’t forget 
+	the images and tables!) [toHTML]
+* Extract all the elements of a given type (e.g. formulas, code) from an ODF
+	document. [getElementsByType]
+* Take an ODF document and extract any images/objects embedded in it. 
+	[getEmbeddedObjects]
 * Iterate over a directory of ODF files and print how many of them were authored
 	by a given person. (list file ownership in a friendly format)
-* Take an ODF word processing document and convert it to HTML. (Don’t forget 
-	the images and tables!)
-* Extract all the elements of a given type (e.g. formulas, code) from an ODF
-	document.
 
 More ideas:
+* Determine document type (in case of wrong filename extension)
 * Clean up styles: If two or more styles have the same attributes, pick one 
 	name for all of them and delete the other styles.
-* Determine document type (in case of wrong filename extension)
-*
-
-
-
+* Diff -- mad useful. In the distant future, it may be possible to re-implement
+	"track changes" as a diff patch to be included in the ODF file.
+* Templating system -- e.g. set up an interface for generating new documents
+	with a predetermined look-and-feel, including headers, footers etc.
+	Also have an interface for generating reports using this system.
 
 """
 
+import os
 import zipfile
 from cStringIO import StringIO
 
@@ -38,37 +42,17 @@ class WriteException: pass
 class ReadException: pass
 
 
-# Provides a Pickle-like interface for reading and writing. 
-
-def Load(src):
-	return _read(src)
-
-def Loads(str):
-	src = StringIO(str)
-	return _read(src)
-
-def Dump(obj, dst):
-	dst.write(_write(obj))
-	return
-
-def Dumps(obj):
-	dst = StringIO()
-	dst.write(_write(obj))
-	return dst.getvalue()
-
-
-# The business end of this ---------------------------------------------------
-
 # Map the names of the Zipped files to attributes of the Document object.
 g_Filenames = {"content.xml":content,
 			 "styles.xml":style,
 			 "meta.xml":meta,
 			 "settings.xml":settings } # mimetype?
 
+
 def _read(src):
 	""" Reads the contents of each of the files in archive src into obj. """
 	zf = ZipFile(src, 'r')
-	obj = None
+	obj = document.Document()
 	for filename, component in g_Filenames:
 		obj.__setattr__(component, zf.read(filename))
 	zf.close()
@@ -78,25 +62,79 @@ def _read(src):
 def _write(obj, dst):
 	""" Writes each of obj's attributes to a Zip file named dst.
 
-	NB: Pardon the mess, this isn't going to be implemented till later.
+	Issue: If a file in the current working directory has the same name as
+	any of the keys in g_Filenames, an exception is thrown to prevent
+	overwriting the file. This is a kludge.
+	Better approaches:
+	- Create a temporary directory for this work (but what to name it?)
+	- Create these files in memory only (how do I do that?)
 	"""
-	#out_stream = obj.toFile() # TODO: implement
 	zf = ZipFile(dst, 'w')
-	#zf.write(filename) # takes the name or an existing file
-	out_stream.close()
+
+	for filename, attribute in g_Filenames:
+		if filename in os.listdir(os.getcwd()):
+			raise WriteException, "Naming conflict -- %s already exists."
+									% filename
+		f = open(filename, 'w') 
+		f.write(obj.getComponentAsString(attribute))
+		f.close()
+		zf.write(filename)
+
 	zf.close()
+	#TODO: Clean up intermediate files
 
 
-# --- Basic test script -------------------------------------------------------
+
+# -----------------------------------------------------------------------------
+# Provides a Pickle-like interface for reading and writing. 
+
+def Load(src):
+	return _read(src)
+
+def Loads(str):
+	src = StringIO(str)
+	obj = _read(src)
+	src.close()
+	return obj
+
+def Dump(obj, dst):
+	dst.write(_write(obj))
+	return
+
+def Dumps(obj):
+	dst = StringIO()
+	dst.write(_write(obj))
+	str = dst.getvalue()
+	dst.close()
+	return str
+
+
+# -----------------------------------------------------------------------------
+# File format conversions
+
+def OdfToText(filename):
+	obj = _read(filename)
+	return obj.toText()
+
+
+def OdfToHTML(filename, title=''):
+	obj = _read(filename)
+	return obj.toHTML(title)
+
+
+# -----------------------------------------------------------------------------
+# Basic test script
 
 if __name__ == "__main__":
 	
-	import os
 	
 	for filename in os.listdir(os.getcwd()):
 		if filename.rsplit('.').pop() in ['odt']:
-			doc = Load(open(filename, 'r'))
-			print doc.getText()
+			print "\n\nContents of %s:" % filename
+			print OdfToText(filename)
+			f = open("%s.html" % filename.rsplit('.',1)[0], 'w')
+			f.write(OdfToHTML(filename))
+			f.close()
 
 
 #EOF

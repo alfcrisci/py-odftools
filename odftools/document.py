1549c47de7e3d57df4aede8895bea3510efd8a9e
rene.leonhardt <rene.leonhardt@4df8ec11-d81d-0410-9be8-316d5d3e3d15>
2006-12-11 07:59:08 +0000

    Fixed: - Variable argument list for Document() - Variables in template.html - dom.unlink() problems for nonexistent attributes - Missing brackets for file.close
    Changed:
    - More extensive use of list comprehensions
    
    Added:
    - Source code encoding
    - Proposal for an OpenDocument Developers Kit (ODDK) (just as a note for now)
    - manifest.xml
    - Check for ZipFile existence
    - dom.Node iterator
    
    TODO:
    - Fix HTML encoding before writing output file

--- odftools/document.py
+++ odftools/document.py
@@ -2,7 +2,7 @@
 
 """ document
 
-The document object, for containing the document in memory and to be used as 
+The document object, for containing the document in memory and to be used as
 the intermediate step for all operations.
 
 This implementation relies on minidom for handling the object. It may later
@@ -12,103 +12,121 @@ should do.
 
 import xml.dom.minidom as dom
 
+
 class Document:
-	""" The ODF document object. """
+  """ The ODF document object. """
+  keys=['mimetype', 'content', 'manifest', 'styles', 'settings', 'meta']
 
-	def __init__(self, 
-				 mimetype='',	# Mimetype string
-				 content='',	# Content data (the text)
-				 manifest='',	# Lists the contents of the ODF file
-				 styles='',		# Formatting data 
-				 settings='',	# Use is specific to the application 
-				 meta='',		# Metadata
-				 **kwords		# Other files in META-INF
-				 ): 
-		self.mimetype = mimetype
-		self.content = dom.parseString(content)
-		self.manifest = dom.parseString(manifest)
-		self.styles = dom.parseString(styles)
-		self.settings = dom.parseString(settings)
-		self.meta = dom.parseString(meta)
-		#ENH: Handle **kwords
-		# --- alternate route --- 
-		# self.Components = {}
+  def __init__(self,
+         mimetype='', # Mimetype string
+         content='',  # Content data (the text)
+         manifest='', # Lists the contents of the ODF file
+         styles='',   # Formatting data
+         settings='', # Use is specific to the application
+         meta='',   # Metadata
+         **kwords   # Other files in META-INF
+         ):
+    args = locals()
+    for key in self.__class__.keys:
+      if key not in args or 0 == len(args[key]):
+        setattr(self, key, '')
+      elif 'mimetype' == key:
+        setattr(self, key, args[key])
+      else:
+        setattr(self, key, dom.parseString(args[key]))
+    #ENH: Handle **kwords
+    # --- alternate route ---
+    # self.Components = {}
 
-	def __del__(self):
-		""" Unlink each DOM component """
-		self.manifest.unlink()
-		self.content.unlink()
-		self.styles.unlink()
-		self.settings.unlink()
-		self.meta.unlink()
-
-
-	# Extract objects from the document --------------------------------------
-
-	def getComponentAsString(self, component_name):
-		nodelist = self.__getattr__(component_name)
-		return nodelist.toprettyxml()
+  def __del__(self):
+    """ Unlink each DOM component """
+    for key in self.__class__.keys:
+      attr = getattr(self, key)
+      if str != type(attr):
+        attr.unlink()
 
 
-	def getEmbeddedObjects(self, filter=None):
-		""" Return a dictionary of the objects embedded in the document.
+  # Extract objects from the document --------------------------------------
 
-		A more general form of getImages. By default, this should return
-		all embedded objects; the list/dictionary can also be filtered
-		for a certain type, e.g. image files.
-		"""
-		pass
-		
-	def getElementsByType(self, elementtype):
-		""" Extract all elements of a given type from the document.
-
-		For example, formulas or code.
-		"""
-		pass
-
-	# Convert the document to other formats ---------------------------------
-
-	def toText(self):
-		""" Returns the content of the document as a plain-text string. """
-		textlist = []
-		for node in self.content:
-			if node.nodeType == node.TEXT_NODE:
-				textlist.append(node.data)
-		return "\n".join(textlist)
+  def getComponentAsString(self, component_name):
+    nodelist = self.__getattr__(component_name)
+    return nodelist.toprettyxml()
 
 
-	def toHTML(self, title=""):
-		""" Returns an HTML representation of the document. 
-		
-		The current version of this function is essentially the same as toText.
-		
-		The next version should:
-		- Create a stylesheet in the header
-		- Apply corresponding style classes to each node
-		
-		After that, this function should also handle:
-		- hyperlinks
-		- images
-		- tables
-		"""
-		values = {}
-		values["Title"] = title		# Title for the page, if applicable
-		bodyList = []	# the lines to insert into the body of the document
-		# Not implemented yet:
-		metaList = [] 	# Meta tags for the header
-		styleList = []	# Stylesheet elements
+  def getEmbeddedObjects(self, filter=None):
+    """ Return a dictionary of the objects embedded in the document.
 
-		# Extract the body of the HTML document
-		for node in self.content:
-			if node.nodeType == node.TEXT_NODE:
-				htmllist.append("<p>%s</p>" % node.data)
-		values["Body"] = "\n".join(htmllist)
+    A more general form of getImages. By default, this should return
+    all embedded objects; the list/dictionary can also be filtered
+    for a certain type, e.g. image files.
+    """
+    pass
 
-		# Apply values to the HTML template
-		f = open("template.html", 'r')
-		htmlTemplate = f.read()
-		f.close
-		return htmlTemplate % values
+  def getElementsByType(self, elementtype):
+    """ Extract all elements of a given type from the document.
+
+    For example, formulas or code.
+    """
+    pass
+
+  # Convert the document to other formats ---------------------------------
+
+  def toText(self):
+    """ Returns the content of the document as a plain-text string. """
+    textlist = [node.data for node in doc_order_iter(self.content) if node.nodeType == node.TEXT_NODE]
+    return "\n".join(textlist)
+
+
+  def toHTML(self, title=""):
+    """ Returns an HTML representation of the document.
+
+    The current version of this function is essentially the same as toText.
+
+    The next version should:
+    - Create a stylesheet in the header
+    - Apply corresponding style classes to each node
+
+    After that, this function should also handle:
+    - hyperlinks
+    - images
+    - tables
+    """
+    values = {}
+    values["title"] = title   # Title for the page, if applicable
+    bodyList = [] # the lines to insert into the body of the document
+    # Not implemented yet:
+    metaList = []   # Meta tags for the header
+    styleList = []  # Stylesheet elements
+
+    # Extract the body of the HTML document
+    htmllist = ["<p>%s</p>" % node.data for node in doc_order_iter(self.content) if node.nodeType == node.TEXT_NODE]
+    values["body"] = "\n".join(htmllist)
+
+    values["meta"] = "" # TODO
+    values["styles"] = "" # TODO
+
+    # Apply values to the HTML template
+    f = open("template.html", 'r')
+    htmlTemplate = f.read()
+    f.close()
+    return htmlTemplate % values
+
+
+# http://www-128.ibm.com/developerworks/library/x-tipgenr.html
+def doc_order_iter(node):
+    """
+    Iterates over each node in document order,
+    returning each in turn
+    """
+    #Document order returns the current node,
+    #then each of its children in turn
+    yield node
+    for child in node.childNodes:
+        #Create a generator for each child,
+        #Over which to iterate
+        for cn in doc_order_iter(child):
+            yield cn
+    return
 
 
 #EOF
